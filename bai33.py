368b61c867facff9a687784253db2f2ecda3885f
da16082005 <hoangducanh160805@gmail.com>
2024-12-29 13:51:06 +0700

    bai33.py

--- bai33.py
+++ bai33.py
@@ -1,8 +1,18 @@
+def is_prime(number):
+    if number <2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+            exit()
+    return True
 m,n = map(int, input().split())
 count=0
 for i in range(m,n+1):
-    for j in range(2,i):
-        if j%i==0:
+    if is_prime(i)== True:
+        count+=1
+print(count)
+        
            
         
             
